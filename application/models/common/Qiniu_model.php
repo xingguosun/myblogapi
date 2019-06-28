355fa288cc5f4e2de5771ee3caa5d1a9aed987ee
Xingguo Sun <xingguosun1024@gmail.com>
2019-06-28 18:53:47 +0800

    update

--- application/models/common/Qiniu_model.php
+++ application/models/common/Qiniu_model.php
@@ -3,8 +3,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Qiniu_model extends Base_Model
 {
-    private $accessKey = "七牛accessKey";
-    private $secretKey = "七牛secretKey";
+    private $accessKey = "zRC851y8hyBAPBUUvxicnzpH6HGTIRUaPwQ_qkij";
+    private $secretKey = "fBMuUr-klXwYc9-d079Xoz-S9qL7HGRZ-_M09adz";
 
     public function __construct()
     {
@@ -20,7 +20,7 @@ class Qiniu_model extends Base_Model
         $deadline = time() + 3600;
         $args['scope'] = $bucket;
         $args['deadline'] = $deadline;
-        $args['returnBody'] = '{"imgUrl": "http://blogimg.codebear.cn/$(key)"}'; // 这里要改成自己的域名或其他，上传成功后是返回这个json数据，url就是图片的地址
+        $args['returnBody'] = '{"imgUrl": "http://idv093d.qiniudns.com/$(key)"}'; // 这里要改成自己的域名或其他，上传成功后是返回这个json数据，url就是图片的地址
         $b = json_encode($args);
         $result = array(
             'token'=> $this->signWithData($b)
